670e615f4af9318fcbebc75c7c2fee24b1ecba7a
Sơn Nguyễn <106434538+1402sn@users.noreply.github.com>
2024-01-21 05:00:22 -0800

    index.js

--- index.js
+++ index.js
@@ -25,20 +25,20 @@ client.on('ready', async () => {
   const r = new Discord.RichPresence()
     .setApplicationId('1116621382466412614')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
+    .setURL('https://youtu.be/ZAPi_WGreGM?si=CTCJA53TFT8LeGYW') //Must be a youtube video link 
     .setState('Solly Is Riel=))')
     .setName('Solly Aka Sully')
     .setDetails(`Valorant [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/935045371393114202/1198569253952045107/ac8e5269067e52df4175d74b21f210fa.jpg?') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Belive in yourself, bro') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/935045371393114202/1198569943986360360/WRr68yodFG2R8Iwr2WgbA7D3t0m3AiMHpymvrJBbY3U.png?') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Maybe me?') //Text when you hover the Small image
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1116694463658598412/1198612468403023922/6edad89ec8ecc9218cee7ef86e2a8630.jpg?') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Belive in yourself, bro!') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1116694463658598412/1198612478318350387/42ba2bcfc3e3024b8683f77d0905e623.jpg?') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Maybe its me?') //Text when you hover the Small image
     .addButton('Watch 👀', 'https://youtube.com/playlist?list=PL954J2wbiE1_Q-UzsL96dbvEQpmVrqAWO&si=m4BPSEUZT728ENyb')
-    .addButton('Hell 👹', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    .addButton('Hell 👹', 'https://discord.com/invite/vn-g');
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
 
   let prevTime = null;
   setInterval(() => {
