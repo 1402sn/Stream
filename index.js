5d698d71c8a8480c4a444d77a6e9619c6dc57e35
Sơn Nguyễn <106434538+1402sn@users.noreply.github.com>
2024-01-22 01:36:24 -0800

    index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}] Sollyy`;
+      const newDetails = `Sollyy [${newTime}] `;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
